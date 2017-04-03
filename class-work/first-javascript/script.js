623cc3b1ac9b98ee3b40f68a73394f9f4118fe21
jainatee <jbteeluck@gmail.com>
2017-04-03 18:29:46 -0400

    startup matchmaker

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,23 @@
+/*document.write(10 * 10);
+document.write(100 / 10);
+document.write(50 - 17);
+document.write(50 + 17);
+document.write('jaina' + 'teeluck');
+
+var num1 = 10 * 10;
+var num2 = 100 / 10;
+var num3 = 50 - 17;
+var num4 = 50 + 17;
+var myName = 'jaina' + 'teeluck'
+
+
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(myName);
+*/
+
+
+var title = document.querySelector('#title').style.color = 'red'
+var title = document.querySelector('h1').style.color = 'blue'
